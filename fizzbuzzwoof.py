6880441099d02207fb1116dad6d703c55fb8a45e
Chanathip Srithanrat <omongo@gmail.com>
2014-06-09 02:03:47 +0700

    Initial commit

---
+++ fizzbuzzwoof.py
@@ -0,0 +1,8 @@
+class FizzBuzzWoof:
+
+    def say(self, number):
+        return 'Fizz' * (not number % 3) + \
+               'Buzz' * (not number % 5) + \
+               'Woof' * (not number % 7) \
+               or str(number)
+
---
+++ fizzbuzzwoof_test.py
@@ -0,0 +1,46 @@
+import unittest
+
+from fizzbuzzwoof import FizzBuzzWoof
+
+class TestFizzBuzzWoof(unittest.TestCase):
+
+    def test_1_should_be_1(self):
+        expected = '1'
+        actual = FizzBuzzWoof().say(1)
+        self.assertEqual(expected, actual)
+
+    def test_3_should_be_Fizz(self):
+        expected = 'Fizz'
+        actual = FizzBuzzWoof().say(3)
+        self.assertEqual(expected, actual)
+
+    def test_5_should_be_Buzz(self):
+        expected = 'Buzz'
+        actual = FizzBuzzWoof().say(5)
+        self.assertEqual(expected, actual)
+
+    def test_7_should_be_Bang(self):
+        expected = 'Woof'
+        actual = FizzBuzzWoof().say(7)
+        self.assertEqual(expected, actual)
+
+    def test_15_should_be_FizzBuzz(self):
+        expected = 'FizzBuzz'
+        actual = FizzBuzzWoof().say(15)
+        self.assertEqual(expected, actual)
+
+    def test_21_should_be_FizzBang(self):
+        expected = 'FizzWoof'
+        actual = FizzBuzzWoof().say(21)
+        self.assertEqual(expected, actual)
+
+    def test_35_should_be_BuzzBang(self):
+        expected = 'BuzzWoof'
+        actual = FizzBuzzWoof().say(35)
+        self.assertEqual(expected, actual)
+
+    def test_105_should_be_FizzBuzzWoofBang(self):
+        expected = 'FizzBuzzWoof'
+        actual = FizzBuzzWoof().say(105)
+        self.assertEqual(expected, actual)
+
